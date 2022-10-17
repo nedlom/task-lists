39ef8b7ca7197f03b2fc9a816a581e5ec7514c27
Nick <nelom3@gmail.com>
2022-10-17 14:27:44 -0400

    Adds new, create and list_params method to controller and new form to view

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,22 @@
 class ListsController < ApplicationController
+
+  def index
+    @lists = List.all 
+  end
+  
+  def new
+    @list = List.new
+  end
+
+  def create
+    List.create(list_params)
+    redirect_to lists_path
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+
 end
